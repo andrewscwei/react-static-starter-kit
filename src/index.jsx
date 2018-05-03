192b4e881112edefe0d20cf4ac87f5d17b92e3ba
Andrew Wei <andrewscwei@gmail.com>
2018-05-03 11:51:35 -0700

    Update linter config

--- src/index.jsx
+++ src/index.jsx
@@ -7,12 +7,11 @@ import routes from './routes';
 import thunk from 'redux-thunk';
 import React from 'react';
 import Router from 'react-router-dom/BrowserRouter';
-import { connect } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { render } from 'react-snapshot';
 import { renderRoutes } from 'react-router-config';
 import { IntlProvider } from 'react-intl';
-import { Provider } from 'react-redux';
 
 const ConnectedIntlProvider = connect((state) => ({ locale: state.i18n.locale, key: state.i18n.locale, messages: state.i18n.messages }))(IntlProvider);
 const store = createStore(combineReducers(reducers), {}, applyMiddleware(thunk));
