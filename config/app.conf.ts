74dfe6d09a94a9934944ce251bd5d4b717f7f322
Andrew Wei <andrewscwei@gmail.com>
2018-09-05 16:53:03 -0700

    Optimizations

--- config/app.conf.ts
+++ config/app.conf.ts
@@ -2,35 +2,45 @@
  * @file Configuration for both client and server environments.
  */
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
+  // Version number.
+  version: require('../package.json').version,
+
+  // Build number.
+  buildNumber: process.env.BUILD_NUMBER || 0,
+
   // HTML metadata.
   meta: {
     // Title of the app.
-    title: `React Static Starter Kit`,
+    title: 'React Static Starter Kit',
 
     // Short description of the app.
-    description: require(`../package.json`).description,
+    description: require('../package.json').description,
 
     // Search keywords.
-    keywords: require(`../package.json`).keywords,
+    keywords: require('../package.json').keywords,
 
     // App URL.
-    url: require(`../package.json`).homepage,
+    url: require('../package.json').homepage,
   },
 
   // Supported locales. First locale is the default locale.
-  locales: [`en`, `ja`],
+  locales: ['en', 'ja'],
 
   // Config options specific to the `build` task.
   build: {
     // Public path of all loaded assets.
-    publicPath: process.env.PUBLIC_PATH || `/`,
+    publicPath: process.env.PUBLIC_PATH || '/',
 
     // Specifies whether JavaScript and CSS source maps should be generated.
     sourceMap: true,
 
     // Specifies whether a bundle analyzer report should be generated at the end
     // of the build process.
-    analyzer: process.env.ANALYZE_BUNDLE === `true` ? true : false,
+    analyzer: process.env.ANALYZE_BUNDLE === 'true' ? true : false,
   },
 };
