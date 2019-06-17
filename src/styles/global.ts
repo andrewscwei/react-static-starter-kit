b8ed013ab0538cf3ab46915079b2cbe8730c1f0e
Andrew Wei <andrewscwei@gmail.com>
2019-06-17 13:33:38 -0700

    Add local fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,10 +5,34 @@ import theme from './theme';
 export default css`
   ${normalize} /* stylelint-disable-line max-empty-lines */
 
+  @font-face {
+    font-family: 'Roboto';
+    src: url('${require('../assets/fonts/Roboto-Bold.ttf')}') format('truetype');
+    font-style: normal;
+    font-weight: 700;
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    src: url('${require('../assets/fonts/Roboto-Regular.ttf')}') format('truetype');
+    font-style: normal;
+    font-weight: 400;
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    src: url('${require('../assets/fonts/Roboto-Light.ttf')}') format('truetype');
+    font-style: normal;
+    font-weight: 300;
+    font-display: fallback;
+  }
+
   html,
   body {
     background: ${theme.backgroundColor};
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Roboto';
     width: 100%;
   }
 
