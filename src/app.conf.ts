a5d0c49ad992181dba8d3b48395bdbe38dc54329
Andrew Wei <andrewscwei@gmail.com>
2020-01-28 08:56:15 -0800

    Reset default `ga` and `gtag` values in app config file

--- src/app.conf.ts
+++ src/app.conf.ts
@@ -14,10 +14,10 @@ export default {
   buildNumber: process.env.BUILD_NUMBER || 0,
 
   // Google Analytics ID (i.e. UA-XXXXXXXX-1)
-  ga: 'UA-XXXXXXXX-1',
+  ga: undefined,
 
   // Google Tag Manager ID (i.e. GTM-XXXXXXX)
-  gtag: 'GTM-XXXXXXX',
+  gtag: undefined,
 
   // HTML metadata.
   meta: {
