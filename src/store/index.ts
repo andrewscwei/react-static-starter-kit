3b136f564aca4a479f12f5c60a85bfd677c6b679
Andrew Wei <andrewscwei@gmail.com>
2020-12-08 15:20:23 -0800

    Upgrade packages

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,14 @@
 import _ from 'lodash';
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import i18n, { I18nState } from './i18n';
-import users, { UsersState } from './users';
+import i18n from './i18n';
+import users from './users';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export interface AppState {
-  i18n: I18nState;
-  users: UsersState;
-}
+export type AppState = NonNullable<Parameters<typeof reducer>[0]>
+
+export type AppAction = NonNullable<Parameters<typeof reducer>[1]>
 
 export const reducer = combineReducers({ i18n, users });
 
