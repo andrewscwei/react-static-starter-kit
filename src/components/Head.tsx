08ae33e096acd38ee958557a9a7223aa49d6179a
Andrew Wei <andrewscwei@gmail.com>
2022-10-05 22:48:00 -0700

    Test Head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -52,15 +52,15 @@ export default function Head({
       <meta name='apple-mobile-web-app-capable' content='yes'/>
       <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'/>
       <meta name='apple-mobile-web-app-title' content={baseTitle}/>
-      <link rel='apple-touch-icon' href='/app-icon-57.png' sizes='57x57'/>
-      <link rel='apple-touch-icon' href='/app-icon-60.png' sizes='60x60'/>
-      <link rel='apple-touch-icon' href='/app-icon-72.png' sizes='72x72'/>
-      <link rel='apple-touch-icon' href='/app-icon-76.png' sizes='76x76'/>
-      <link rel='apple-touch-icon' href='/app-icon-114.png' sizes='114x114'/>
-      <link rel='apple-touch-icon' href='/app-icon-120.png' sizes='120x120'/>
-      <link rel='apple-touch-icon' href='/app-icon-144.png' sizes='144x144'/>
-      <link rel='apple-touch-icon' href='/app-icon-152.png' sizes='152x152'/>
-      <link rel='apple-touch-icon' href='/app-icon-180.png' sizes='180x180'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='57x57'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='60x60'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='72x72'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='76x76'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='114x114'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='120x120'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='144x144'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='152x152'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='180x180'/>
       <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='192x192'/>
       <link rel='manifest' href='/manifest.json'/>
     </Helmet>
