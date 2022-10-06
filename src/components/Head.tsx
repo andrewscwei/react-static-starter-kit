16cf2416b1216a7eafa5ec666916cba7497db1bf
Andrew Wei <andrewscwei@gmail.com>
2022-10-06 15:11:53 -0700

    Use JS to adjust favicons

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useLocation } from 'react-router'
 import * as assets from '../assets'
@@ -18,13 +18,27 @@ export default function Head({
   const pageDescription = description ?? baseDescription
   const pageUrl = baseUrl + useLocation().pathname
   const locale = useLocale()
+  const [isDarkMode, setIsDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+  const colorSchemeChangeHandler = (event: MediaQueryListEvent) => setIsDarkMode(event.matches)
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+
+    const media = window.matchMedia('(prefers-color-scheme: dark)')
+    media.addEventListener('change', colorSchemeChangeHandler)
+
+    return () => {
+      media.removeEventListener('change', colorSchemeChangeHandler)
+    }
+  }, [])
 
   return (
     <Helmet>
       <link rel='canonical' href={pageUrl}/>
-      <link rel='mask-icon' type='image/svg+xml' href={assets.meta.PinnedIcon} color='#000'/>
-      <link rel='alternate icon' type='image/png' href={assets.meta.AltFaviconLight}/>
-      <link rel='icon' type='image/svg+xml' href={assets.meta.FaviconDark}/>
+      <link rel='mask-icon' type='image/svg+xml' href={assets.meta.PinnedIcon} color={isDarkMode ? '#fff' : '#000'}/>
+      <link rel='alternate icon' type='image/png' href={isDarkMode ? assets.meta.AltFaviconDark : assets.meta.AltFaviconLight}/>
+      <link rel='icon' href={isDarkMode ? assets.meta.FaviconDark : assets.meta.FaviconLight}/>
       <title>{pageTitle}</title>
       <meta name='description' content={pageDescription}/>
 
