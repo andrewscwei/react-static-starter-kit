0f633c1ac2be583913abee7d14643b6627914442
Andrew Wei <andrewscwei@gmail.com>
2022-10-05 22:44:45 -0700

    Test Head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -52,7 +52,6 @@ export default function Head({
       <meta name='apple-mobile-web-app-capable' content='yes'/>
       <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'/>
       <meta name='apple-mobile-web-app-title' content={baseTitle}/>
-      <link rel='apple-touch-icon' href={assets.meta.AltFavicon}/>
       <link rel='apple-touch-icon' href='/app-icon-57.png' sizes='57x57'/>
       <link rel='apple-touch-icon' href='/app-icon-60.png' sizes='60x60'/>
       <link rel='apple-touch-icon' href='/app-icon-72.png' sizes='72x72'/>
@@ -62,7 +61,7 @@ export default function Head({
       <link rel='apple-touch-icon' href='/app-icon-144.png' sizes='144x144'/>
       <link rel='apple-touch-icon' href='/app-icon-152.png' sizes='152x152'/>
       <link rel='apple-touch-icon' href='/app-icon-180.png' sizes='180x180'/>
-      <link rel='apple-touch-icon' href='/app-icon-192.png' sizes='192x192'/>
+      <link rel='apple-touch-icon' href={assets.meta.AltFavicon} sizes='192x192'/>
       <link rel='manifest' href='/manifest.json'/>
     </Helmet>
   )
