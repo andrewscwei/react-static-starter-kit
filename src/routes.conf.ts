f5c259b1c68bb2aba64cac8d2c6b7913ec14656f
Andrew Wei <andrew@sybl.io>
2023-09-01 12:20:24 -0700

    Add ErrorBoundary

--- src/routes.conf.ts
+++ src/routes.conf.ts
@@ -3,15 +3,20 @@
  */
 
 import type { RouteObject } from 'react-router'
+import { ErrorBoundary } from './ui/ErrorBoundary'
 
 export const config: RouteObject[] = [{
-  path: '/',
-  index: true,
-  lazy: () => import('./ui/pages/home'),
-}, {
-  path: '/quote',
-  lazy: () => import('./ui/pages/quote'),
-}, {
-  path: '*',
-  lazy: () => import('./ui/pages/notFound'),
+  id: 'root',
+  ErrorBoundary,
+  children: [{
+    path: '/',
+    index: true,
+    lazy: () => import('./ui/pages/home'),
+  }, {
+    path: '/quote',
+    lazy: () => import('./ui/pages/quote'),
+  }, {
+    path: '*',
+    lazy: () => import('./ui/pages/notFound'),
+  }],
 }]
