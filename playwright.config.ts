542ea25bcb5c10df858196cd5c2517a626cb3392
Andrew Wei <andrewscwei@gmail.com>
2024-08-01 00:10:23 -0700

    build: Optimize playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig, devices } from '@playwright/test'
 
+const CI = !!process.env.CI
+const PORT = process.env.PORT || '8080'
+
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: CI,
+  retries: CI ? 2 : 0,
+  workers: CI ? 1 : undefined,
   reporter: 'list',
   use: {
-    baseURL: 'http://localhost:8080',
+    baseURL: `http://localhost:${PORT}`,
     trace: 'on-first-retry',
   },
   projects: [{
@@ -28,8 +31,8 @@ export default defineConfig({
     use: { ...devices['iPhone 12'] },
   }],
   webServer: {
-    command: 'npm start',
-    url: 'http://localhost:8080',
-    reuseExistingServer: !process.env.CI,
+    command: `npm run start:${process.env.RUNTIME || 'server'}`,
+    url: `http://localhost:${PORT}`,
+    reuseExistingServer: CI,
   },
 })
