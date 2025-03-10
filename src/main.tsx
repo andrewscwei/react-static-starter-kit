53282ea1d4f92dcf730df47533daddd4bde1bcab
Andrew Wei <andrewscwei@gmail.com>
2025-03-10 08:56:51 -0700

    refactor: Optimize routes config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { debug } from '@lib/debug'
-import { generateLocalizedRoutes } from '@lib/i18n'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
 import { BASE_PATH } from './app.config.js'
-import { i18n } from './i18n.config.js'
 import { routes } from './routes.config.js'
 import { App } from './ui/App.js'
 import WebWorker from './workers/web.js?worker'
@@ -24,11 +22,9 @@ function render() {
 
   if (!container) throw Error('Invalid application root')
 
-  const localizedRoutes = generateLocalizedRoutes(routes, i18n)
-
   createRoot(container).render((
     <App>
-      <RouterProvider router={createBrowserRouter(localizedRoutes, { basename: BASE_PATH })}/>
+      <RouterProvider router={createBrowserRouter(routes, { basename: BASE_PATH })}/>
     </App>
   ))
 
