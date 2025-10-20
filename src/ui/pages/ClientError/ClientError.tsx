6d1fa6df2ff81dd5ec9ed286cc345731fe764290
Andrew Wei <andrewscwei@gmail.com>
2025-10-20 17:06:17 +0900

    feat: Add ClientError page

--- src/ui/pages/ClientError/ClientError.tsx
+++ src/ui/pages/ClientError/ClientError.tsx
@@ -5,9 +5,14 @@ export function Component() {
   const ltxt = useLocalizedString()
 
   return (
-    <Page metadata={{ noIndex: true, title: ltxt('window-title-not-found') }}>
+    <Page
+      metadata={{
+        noIndex: true,
+        title: ltxt('window-title-client-error'),
+      }}
+    >
       <main>
-        <h1>{ltxt('not-found-title')}</h1>
+        <h1>{ltxt('client-error-title')}</h1>
       </main>
     </Page>
   )
