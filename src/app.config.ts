12a57eb2a890935230816594ebb2d3065b37d764
Andrew Wei <andrewscwei@gmail.com>
2026-04-19 11:29:58 -0700

    feat: Update app config

--- src/app.config.ts
+++ src/app.config.ts
@@ -7,6 +7,5 @@ export const BASE_PATH = import.meta.env.BASE_PATH
 export const BASE_URL = import.meta.env.BASE_URL
 export const DEBUG = import.meta.env.DEBUG
 export const DEFAULT_LOCALE = import.meta.env.DEFAULT_LOCALE
-
-/** App config */
-export const VERSION = `v${import.meta.env.VERSION}+build.${import.meta.env.BUILD_NUMBER.slice(0, 7)}`
+export const VERSION = import.meta.env.VERSION
+export const BUILD_NUMBER = import.meta.env.BUILD_NUMBER
